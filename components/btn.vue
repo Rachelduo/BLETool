9fc307db52ffe016fa788cfa83b26157949acfcb
Rachelduo <gd19891209@163.com>
2026-05-15 17:57:28 +0800

    first commit

---
+++ components/btn.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class='btn-box' :style="{color:color}" @click="OnClick">
+		<slot></slot>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"btn",
+		props:{
+			color:{
+				type:String,
+				default:'000'
+			}
+		},
+		data(){
+			return{};
+		},
+		methods:{
+			OnClick(){
+				console.log('我被点击了')
+				this.$emit('change',this.color)
+			}
+		}
+		
+	}
+</script>
+
+<style>
+     .btn-box{
+		 width:200px;
+		 height:100px;
+		 text-align:center;
+		 line-height:100px;
+		 background-color: aqua;
+		
+	 }
+</style>
